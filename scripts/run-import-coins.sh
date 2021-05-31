f851b9033cfcdf4719c9c1e23bfd6a00259ac0be
Dominic Kolbe <kolbedominic@gmail.com>
2021-05-31 18:12:36 +0100

    remove NODE_ENV from script

--- scripts/run-import-coins.sh
+++ scripts/run-import-coins.sh
@@ -2,6 +2,6 @@
 
 echo "# # # Start import coins # # #"
 
-sudo docker exec -t coins-server NODE_ENV=production yarn workspace coins-server ts-node --transpile-only ./src/tasks/run-import-coins.ts
+sudo docker exec -t coins-server yarn workspace coins-server ts-node --transpile-only ./src/tasks/run-import-coins.ts
 
 echo "# # # Finished import coins # # #"

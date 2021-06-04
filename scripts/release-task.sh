dd789ca281269e7a0f45ca504e8e2a81208887b4
Dominic Kolbe <kolbedominic@gmail.com>
2021-06-04 19:06:46 +0100

    Remove sudo

--- scripts/release-task.sh
+++ scripts/release-task.sh
@@ -3,19 +3,19 @@
 echo "# # # Start release task # # #"
 
 echo "1. Stop docker instance"
-sudo docker-compose --env-file packages/coins-server/.env down
+docker-compose --env-file packages/coins-server/.env down
 
 echo "2. Fetch repository ..."
 git fetch origin
 git reset --hard origin/master
 
 echo "3. make scripts executable"
-sudo chmod +x scripts/*.sh
+chmod +x scripts/*.sh
 
 echo "4. Rebuild docker instance"
-sudo docker-compose --env-file packages/coins-server/.env build
+docker-compose --env-file packages/coins-server/.env build
 
 echo "5. Restart docker instance"
-sudo docker-compose --env-file packages/coins-server/.env up -d
+docker-compose --env-file packages/coins-server/.env up -d
 
 echo "# # # Finished release task # # #"

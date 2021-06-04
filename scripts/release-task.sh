5e536aa91a714702db2a0c12f035181a4f235828
Dominic Kolbe <kolbedominic@gmail.com>
2021-06-04 20:02:36 +0100

    Update release script

--- scripts/release-task.sh
+++ scripts/release-task.sh
@@ -3,19 +3,19 @@
 echo "# # # Start release task # # #"
 
 echo "1. Stop docker instance"
-docker-compose --env-file packages/coins-server/.env down
+sudo docker-compose --env-file packages/coins-server/.env down
 
 echo "2. Fetch repository ..."
-git fetch origin
-git reset --hard origin/master
+sudo git fetch origin
+sudo git reset --hard origin/master
 
 echo "3. make scripts executable"
-chmod +x scripts/*.sh
+sudo chmod +x scripts/*.sh
 
 echo "4. Rebuild docker instance"
-docker-compose --env-file packages/coins-server/.env build
+sudo docker-compose --env-file packages/coins-server/.env build
 
 echo "5. Restart docker instance"
-docker-compose --env-file packages/coins-server/.env up -d
+sudo docker-compose --env-file packages/coins-server/.env up -d
 
 echo "# # # Finished release task # # #"
